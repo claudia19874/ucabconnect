a211b2505e09fa5d857dcd8685879d682c3613cd
claudia19874 <chrojas.24@est.ucab.edu.ve.com>
2026-05-24 23:44:32 -0400

    Mejorada

--- frontend/src/app/features/perfiles/registro-estudiante/registro-estudiante.component.ts
+++ frontend/src/app/features/perfiles/registro-estudiante/registro-estudiante.component.ts
@@ -12,22 +12,25 @@ import { RouterLink, Router } from '@angular/router';
 })
 export class RegistroEstudianteComponent {
   
-  // Modelo actualizado con la información académica correcta
   estudiante = {
     nombreCompleto: '',
     cedula: '',
     correo: '',
     contrasena: '',
-    carrera: '',      // 2. Campo añadido para la carrera que estudia
-    semestre: '',     // 2. Inicializado como string vacío para mejor manejo del placeholder
+    carrera: '',
+    semestre: '',
     habilidades: '',
-    intereses: ''
+    intereses: '',
+    sede: 'Caracas' // Valor inicial por defecto
   };
 
   constructor(private router: Router) {}
 
+  setSede(sede: string) {
+    this.estudiante.sede = sede;
+  }
+
   onRegistro() {
-    // 3. Validación de seguridad extra en la lógica por si se salta el HTML
     if (!this.estudiante.correo.endsWith('@est.ucab.edu.ve')) {
       alert('Por favor, ingresa un correo institucional válido (@est.ucab.edu.ve).');
       return;
